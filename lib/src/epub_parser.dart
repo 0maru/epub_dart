efd24f6e44312d16d0a658642388d1c93a806f82
0maru <0maru.dev@gmail.com>
2023-08-11 12:29:46 +0900

    remove method

--- lib/src/epub_parser.dart
+++ lib/src/epub_parser.dart
@@ -1,5 +1,1 @@
-class EpubParser {
-  static Future<void> fromFile(String path) async {}
-  static Future<void> fromString(String data) async {}
-  static Future<void> fromNetwork(String url) async {}
-}
+class EpubParser {}
